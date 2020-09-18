22e512eae2a2a9ba0fefab96c983e5f1e6b5b6a3
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-18 22:13:24 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -3,20 +3,34 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "react-bootstrap";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <div className="container-id">
-                    <center>
-                        <input id="input-id" placeholder="Add to the list" />
-                    </center>
-                    <Button className="btn">ADD</Button>
-                </div>
-            </header>
-        </div>
-    );
+export function add() {
+    console.log("button clicked");
 }
 
-export default App;
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <div className="container-id">
+                        <center>
+                            <input
+                                id="input-id"
+                                placeholder="Add to the list"
+                            />
+                        </center>
+                        <Button className="btn" onClick={() => add()}>
+                            ADD
+                        </Button>
+                    </div>
+                </header>
+            </div>
+        );
+    }
+}
