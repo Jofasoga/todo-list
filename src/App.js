2c238cdccd7166d48155311d7c8f79430b84805c
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-18 19:25:13 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <input id="input" placeholder="Add to the list" />
+                <img src={logo} className="App-logo" alt="logo" />
+                <div className="input">
+                    <center>
+                        <input id="input-id" placeholder="Add to the list" />
+                    </center>
+                    <Button className="btn">ADD</Button>
+                </div>
             </header>
         </div>
     );
