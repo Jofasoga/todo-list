12559db1d8debc3a35038ce2a1981114aaba90d7
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-19 23:43:27 +0100

    Updates

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
 	path('task-list/', views.taskList, name="task-list"),
 	path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
 	path('task-create/', views.taskCreate, name="task-create"),
-
 	path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
 	path('task-delete/<str:pk>/', views.taskDelete, name="task-delete"),
 ]
