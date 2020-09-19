186570381cae0ddff7f255d0f2d30b1891c0f1c0
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-19 12:08:09 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default class App extends React.Component {
         super(props);
         this.state = {};
     }
-    
+
     render() {
         return (
             <div className="App">
