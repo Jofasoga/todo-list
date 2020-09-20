4ecc2a8bd8444734108943931660406f18ff2055
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-20 02:47:04 +0100

    Made the app work thanks to a youtube tutorial

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ class App extends React.Component {
             editing: false,
         };
         this.fetchTasks = this.fetchTasks.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentWillMount() {
@@ -23,18 +25,58 @@ class App extends React.Component {
     fetchTasks() {
         console.log("Fetching...");
         fetch("http://127.0.0.1:8000/task-list/")
-            .then((response) => response.json)
-            .then((data) => console.log("Data", data));
+            .then((response) => response.json())
+            .then((data) =>
+                this.setState({
+                    todoList: data,
+                })
+            );
     }
+
+    handleChange(e) {
+        let name = e.target.name;
+        let value = e.target.value;
+        console.log("Name:", name);
+        console.log("Value:", value);
+        this.setState({
+            activeItem: {
+                ...this.state.activeItem,
+                title: value,
+            },
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log("ITEM: ", this.state.activeItem);
+        let url = "http://127.0.0.1:8000/task-create/";
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(this.state.activeItem),
+        })
+            .then((response) => {
+                this.fetchTasks();
+                this.setState({});
+            })
+            .catch(function (error) {
+                console.log("ERROR:", error);
+            });
+    }
+
     render() {
+        let tasks = this.state.todoList;
         return (
             <div className="container">
                 <div id="task-container">
                     <div id="form-wrapper">
-                        <form id="form">
+                        <form onSubmit={this.handleSubmit} id="form">
                             <div className="flex-wrapper">
                                 <div style={{ flex: 6 }}>
                                     <input
+                                        onChange={this.handleChange}
                                         className="form-control"
                                         id="title"
                                         type="text"
@@ -52,7 +94,20 @@ class App extends React.Component {
                             </div>
                         </form>
                     </div>
-                    <div id="list-wrapper"></div>
+                    <div id="list-wrapper">
+                        {tasks.map(function (task, index) {
+                            return (
+                                <div
+                                    key={index}
+                                    className="task-wrapper flex wrapper"
+                                >
+                                    <div style={{ flex: 7 }}>
+                                        <span>{task.title}</span>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         );
